f3b045d04078a2dcdcb5a1414d6914fe2c22c1c6
Eduard <daniil.korshackov@yandex.ru>
2016-01-19 01:55:01 +0300

    form, stellar.js, arrow-svg

--- html/js/common.js
+++ html/js/common.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+	$.stellar({
+		responsive: true,
+		horizontalOffset: 60
+	});
+
 	function wResize() {
 		$('header').css('min-height', $(window).height());
 	};
